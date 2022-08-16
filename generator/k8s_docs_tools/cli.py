c9f72aeeb60cb486e59b5c5d0408a46c37afe8a2
Adam Dyess <adam.dyess@canonical.com>
2022-08-16 10:58:06 -0500

    Auto generate release content: release-notes, supported-versions, install manual, upgrading, and site index

--- generator/k8s_docs_tools/cli.py
+++ generator/k8s_docs_tools/cli.py
@@ -3,8 +3,9 @@
 
 import click
 import logging
+from pathlib import Path
 
-from k8s_docs_tools.generate_release import generate_component_page
+from k8s_docs_tools.generate_release import generate_release_docs
 
 FORMAT = "%(name)s %(levelname)s: %(message)s"
 logging.basicConfig(format=FORMAT)
@@ -22,9 +23,11 @@ def cli():
     type=str,
     help="Version identifing the stable charmed-kubernetes release.",
 )
-@click.argument("output", type=click.File("w"))
-def component(release, output):
-    generate_component_page(release, output)
+@click.argument("output", type=Path)
+def generate_release(release, output):
+    destination = Path(output)
+    destination.mkdir(exist_ok=True)
+    generate_release_docs(release, destination)
 
 
 if __name__ == "__main__":
